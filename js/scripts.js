18984b2b8917e0c7374a84e8ed8ae574ff33a950
Dan Wright and Lizzie Koehler <muscle.crab.13@gmail.com>
2015-02-17 14:34:35 -0800

    add styling

--- js/scripts.js
+++ js/scripts.js
@@ -6,21 +6,21 @@ $(document).ready(function() {
     var inputHeight = parseInt($("input#height").val());
     var inputWidth = parseInt($("input#width").val());
     var inputLength = parseInt($("input#length").val());
-    var parcel = { shipTo: inputShipTo,
-                    weight: inputWeight,
-                    height: inputHeight,
-                    width: inputWidth,
-                    length: inputLength,
-                    volume: function()  {
-                      return (this.height * this.width * this.length);
-                    },
-                    cost: function() {
-                      return (this.volume() * this.weight);
-                    }
-                    };
+    var parcel = {
+      shipTo: inputShipTo,
+      weight: inputWeight,
+      height: inputHeight,
+      width: inputWidth,
+      length: inputLength,
+      volume: function()  {
+        return (this.height * this.width * this.length);
+      },
+      cost: function() {
+        return (this.volume() * this.weight);
+      }
+    };
 
     var newParcel = {cost: parcel.cost()};
-
-    $("ul#packageInfo").append("<li>" + newParcel.cost + "</li>");
+    $("div#distanceSpacer").append("<div style='width: " + parcel.shipTo + "px; height: 50px; float: left; background-color: hsla(150, 100%, 100%, 0.43);'></div><div width='300'><h1>Your Parcel Will Cost " + newParcel.cost + " Coins</h1></div>");
   });
 });
