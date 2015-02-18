0f46213896f0bdce26efe69e707b047c77d7da92
Dan Wright and Lizzie Koehler <lizziefkoehler@gmail.com>
2015-02-17 17:00:20 -0800

    minor visual refinements

--- js/scripts.js
+++ js/scripts.js
@@ -5,26 +5,26 @@ $(document).ready(function() {
     var inputWeight = parseInt($("input#weight").val());
     var inputHeight = parseInt($("input#height").val());
     var inputWidth = parseInt($("input#width").val());
-    var inputLength = parseInt($("input#length").val());
     var parcel = {
       shipTo: inputShipTo,
       weight: inputWeight,
       height: inputHeight,
-      width: inputWidth,
-      length: inputLength,
-      volume: function()  {
-        return (this.height * this.width * this.length);
+      area: function()  {
+        return (this.height * this.width);
       },
       cost: function() {
         return (this.weight * 10 * this.shipTo / 1000 );
       }
     };
+
+    $("div#distanceSpacer").empty();
+
     var newParcel = {cost: parcel.cost()};
     if (inputShipTo > 12500) {
       alert("This shipping job would require Warp Zone, and that's cheating.");
     } else {
 
-    $("div#distanceSpacer").append("<div id='path' style='width: " + parcel.shipTo + "px;'></div><div width='300'><img src='img/parcel.png' style='height: 200px; width: 200px;'><br><h1> Your Shipment Will Cost " + newParcel.cost + " Coins</h1></div>");
+    $("div#distanceSpacer").append("<div id='path' style='width: " + parcel.shipTo + "px;'></div><div id='shippingLabel'><img src='img/parcel.gif' style='height: " + (parcel.height*3) + "px; width: " + (parcel.width*3) + "px;'><br><h2> Your Shipment Will Cost " + newParcel.cost + " Coins<br><small>Processing surcharges may apply.</small></h2></div>");
     }
 
 
