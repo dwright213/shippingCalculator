0b6898919af13970fe2b5cace3dc9fa76da6ccba
Dan Wright and Lizzie Koehler <lizziefkoehler@gmail.com>
2015-02-17 16:02:07 -0800

    add arrows

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,18 @@ $(document).ready(function() {
         return (this.height * this.width * this.length);
       },
       cost: function() {
-        return (this.volume() * this.weight);
+        return (this.weight * 10 * this.shipTo / 1000 );
       }
     };
-
     var newParcel = {cost: parcel.cost()};
-    $("div#distanceSpacer").append("<div style='width: " + parcel.shipTo + "px; height: 50px; float: left; background-color: hsla(150, 100%, 100%, 0.43);'></div><div width='300'><h1>Your Parcel Will Cost " + newParcel.cost + " Coins</h1></div>");
+    if (inputShipTo > 12500) {
+      alert("This shipping job would require Warp Zone, and that's cheating.");
+    } else {
+
+    $("div#distanceSpacer").append("<div id='path' style='width: " + parcel.shipTo + "px;'></div><div width='300'><img src='img/parcel.png' style='height: 200px; width: 200px;'><br><h1> Your Shipment Will Cost " + newParcel.cost + " Coins</h1></div>");
+    }
+
+
+
   });
 });
